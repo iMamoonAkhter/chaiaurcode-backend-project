f0c07d4b08e23d579982c76bd9a1d4d079e27bbd
imamoonakhter <imamoonakhter@gmail.com>
2025-01-27 01:50:05 +0500

    Models writing and pre methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
-//connectDB()
+
+
+app.listen(process.env.port || 8000, ()=>{
+    console.log(`Server is running at port : ${process.env.PORT || 8000}`)
+})
+
+/*
+connectDB()
+.then(()=>{
+    app.listen(process.env.port || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!!", err)
+})
+*/
 
 
 
@@ -16,25 +33,3 @@ dotenv.config({
 
 
 
-
-
-
-/*import express from "express";
-const app = express();
-(async ()=>{
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error)=>{
-            console.log("Error: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, ()=>{
-            console.log(`Server is running on ${process.env.PORT}`)
-        })
-        
-    } catch (error) {
-        console.error("Error: ", error);
-        throw error
-    }
-})()*/
